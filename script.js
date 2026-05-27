6d5d5b06bc0159adc22720d1793b876eb99fc964
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2026-05-27 21:43:46 +0100

    UI with HTML and CSS sytling done

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adding in a queryselector for all buttons and the calculator screen
+const allButtons = document.querySelector('.all-buttons');
+const calcScreen = document.querySelector('#calc-screen');
+
