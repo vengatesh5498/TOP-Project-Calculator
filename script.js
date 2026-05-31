de54c57d50c1285f7274b5d53de63cfa151d3527
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2026-05-31 22:06:29 +0100

    HTML and CSS completed

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// Declaring empty variables to capture and store the data for the 2 operands and operator
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let isCalculationFinished = false;//To determine if there is a result on the display of the calculator
+
 // Adding in a queryselector for all buttons and the calculator screen
-const allButtons = document.querySelector('.all-buttons');
-const calcScreen = document.querySelector('#calc-screen');
+const calcScreen = document.querySelector('#calc-screen'); /*selecting the display screen id declared in the HTML*/
+const allButtons = document.querySelector('.all-buttons'); /*selectiing the all-buttons class declared in the HTML*/
+
+// Function to perform mathematical logic
+
+function add (a,b) {return a+b;}
+function subract (a,b) {return a-b;}
+function multiply (a,b) {return a*b;}
 
