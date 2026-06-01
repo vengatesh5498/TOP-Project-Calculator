457d48a19794a1f4a08c95a9597fdbbb231040ca
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2026-06-01 06:46:07 +0100

    updateDisplayValue function completed

--- script.js
+++ script.js
@@ -21,4 +21,29 @@ function divide (a,b){
     return a/b;
 } 
 
+// Function that pricks the keyboard string symbols + - / * and performs the mathematical function
+function operate (op, num1, num2){
+    switch op {
+        case '+': return add(num1, num2);
+        case '-': return subract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+        default: return num2;
+    }
+}
 
+// Function for clearing the complex numbers like 4.546687465 and rounding them 
+//Without this function, if you typed 0.1 + 0.2 into your calculator, the screen would display 0.30000000000000004
+function updateDisplayvalue (value){
+    if (isNaN(value)){
+        calcScreen.textContent=value;
+        return; //if the value is not a number returns the error message declared in the divide function
+    }
+    if(!Number.isInteger(Number(value))){
+        calcScreen.textContent=Number(value).toFixed(4).replace(/\.?0+$/,"");
+        //the above line of code rounds the numbers after decimal to 4 and removes extra 0's and gives only clean round number like 4, 8.5 rather than 4.000 and 8.500
+    } else{
+        calcScreen.textContent = value;
+    }
+
+}
